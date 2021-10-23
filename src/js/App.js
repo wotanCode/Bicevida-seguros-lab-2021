53bd8ca482d4e9ef839efedd95d3d993cdbd9987
Pedro Yanez <pedro_y91@outlook.com>
2021-10-23 13:20:51 -0300

    feat/plantilla-base-lista

---
+++ src/js/App.js
@@ -0,0 +1,20 @@
+//css
+import "../css/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bicevida</h1>
+      <select name="select">
+        <option value="value1">Value 1</option>
+        <option value="value2">Value 2</option>
+      </select>
+      <button>Agregar</button>
+    </div>
+  );
+}
+/*
+https://dn8mlk7hdujby.cloudfront.net/interview/insurance/59
+*/
+
+export default App;
