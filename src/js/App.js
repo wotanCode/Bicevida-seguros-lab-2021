65ce0075755ca6896f516cf87bd0e30759efff0a
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2021-10-24 12:36:53 -0300

    feat/obtengo los datos de los seguros a traves de un fetch

--- src/js/App.js
+++ src/js/App.js
@@ -1,15 +1,61 @@
+//React
+import React, { useState, useEffect } from "react";
 //css
 import "../css/App.css";
 
 function App() {
+  const URLapi = "https://dn8mlk7hdujby.cloudfront.net/interview/insurance/";
+  const [seguro1, setSeguro1] = useState();
+  const [seguro2, setSeguro2] = useState();
+
+  //funcion Connexion a la api
+  const fetchseguro = async (idseguro) => {
+    await fetch(`${URLapi}${idseguro}`, {
+      method: "GET",
+    })
+      .then((response) => {
+        // console.log(response.ok); // Será true (verdad) si la respuesta es exitosa.
+        // console.log(response.status); // el código de estado = 200 o código = 400 etc.
+        return response.json();
+      })
+      .then((data) => {
+        if (idseguro === 58) {
+          console.log("Respuesta de la data externa--->", data);
+          setSeguro1(data);
+          console.log("Fin del useEffect");
+        }
+        if (idseguro === 59) {
+          console.log("Respuesta de la data externa--->", data);
+          setSeguro2(data);
+          console.log("Fin del useEffect");
+        }
+      })
+      //mostrarnos un error en caso de que lo tuvieramos
+      .catch((err) => console.log("muestro error -->", err));
+  };
+
+  useEffect(() => {
+    //Llamando a la API con useEffect
+    fetchseguro(58);
+    fetchseguro(59);
+  }, []);
+
   return (
     <div className="App">
       <h1>Bicevida</h1>
-      <select name="select">
-        <option value="value1">Value 1</option>
-        <option value="value2">Value 2</option>
-      </select>
+      <form>
+        {seguro1 && seguro2 ? (
+          <select name="select">
+            <option value="58">{seguro1.insurance.name}</option>
+            <option value="59">{seguro2.insurance.name}</option>
+          </select>
+        ) : (
+          <select name="select"></select>
+        )}
+      </form>
+
       <button>Agregar</button>
+      <button onClick={() => console.log(seguro1)}>mostrar arreglo</button>
     </div>
   );
 }
