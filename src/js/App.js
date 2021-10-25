9bf901f105eecff32b83ef8a3849fbc7e5242868
Pedro Yanez <pedro_y91@outlook.com>
2021-10-25 03:32:07 -0300

    feat/avanzando en el css

--- src/js/App.js
+++ src/js/App.js
@@ -1,9 +1,9 @@
 //React
-import React, { useState, useEffect, useCallback} from "react";
+import React, { useState } from "react";
 //css
 import "../css/App.css";
-//URL API
-const URLapi = "https://dn8mlk7hdujby.cloudfront.net/interview/insurance/";
+//Componentes importados
+import { SeguroCard } from "./components/SeguroCard";
 
 function App() {
   //Lista de seguros disponibles
@@ -19,68 +19,26 @@ function App() {
   //State para el button del seguro, se modifiara cuando se haga click en el button
   const [buttonSeguroSelected, setButtonSeguroSelected] = useState(0);
 
-
-  
-
-  //Componente para la carta del seguro
-  const SeguroCard = (props) => {
-    //Aqui se guarda el seguro extraido del fetch
-    const [seguro, setSeguro] = useState();
-
-    //funcion Connexion al endpoint
-    const fetchseguro = async (idseguro) => {
-      await fetch(`${URLapi}${idseguro}`, {
-        method: "GET",
-      })
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          setSeguro(data);
-        })
-        //mostrarnos un error en caso de que lo tuvieramos
-        .catch((err) => console.log("Muestro error -->", err));
-    };
-
-    //Conectando a la API apenas se inicie el componente
-    useEffect(() => {
-      fetchseguro(props.buttonSeguroSelected);
-    }, [props.buttonSeguroSelected]);
-
-    return <div>{seguro ? (
-      <>
-        <p>{seguro.insurance.name}</p>
-        <p>{seguro.insurance.description}</p>
-        <p>{seguro.insurance.price}</p>
-        <p>{seguro.insurance.image}</p>
-      </>
-      ): null}</div>;
-  };
-
   //Asignamos al estado del select cuando este cambia
   const handleSelectSeguro = (e) => {
     setSeguroIdSelected(e.target.value);
   };
+
   //Asignamos al estado del button cuando este cambia
   const handleButtonSeguro = (e) => {
     e.preventDefault();
     setButtonSeguroSelected(seguroIdSelected);
   };
 
-
-
-
-
-
-
   return (
-    <div className="App">
+    <main className="App">
       <h1>bicevida new</h1>
       <form>
         <select
           onChange={handleSelectSeguro}
           name="seguro"
           value={seguroIdSelected}
+          className="selectstyle"
         >
           {seguroLista.map((seguro, index) => (
             <option key={index} value={seguro.value} disabled={seguro.disabled}>
@@ -88,14 +46,18 @@ function App() {
             </option>
           ))}
         </select>
-        <button type="Submit" onClick={(e) => handleButtonSeguro(e)}>
-          Agregar
+        <button
+          className="buttonSeguro"
+          type="Submit"
+          onClick={(e) => handleButtonSeguro(e)}
+        >
+          Ver
         </button>
       </form>
       {buttonSeguroSelected ? (
         <SeguroCard buttonSeguroSelected={buttonSeguroSelected} />
       ) : null}
-    </div>
+    </main>
   );
 }
 
