f68f15a5c0a13864205f2937b1f44028b0f3c798
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2021-10-25 00:55:41 -0300

    feat/aplicacion funcionando sin diseño

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,5 @@
 //React
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback} from "react";
 //css
 import "../css/App.css";
 //URL API
@@ -19,56 +19,60 @@ function App() {
   //State para el button del seguro, se modifiara cuando se haga click en el button
   const [buttonSeguroSelected, setButtonSeguroSelected] = useState(0);
 
-  //Estas guardan los posibles seguros:
-  const [seguro1, setSeguro1] = useState();
-  const [seguro2, setSeguro2] = useState();
 
-  //funcion Connexion a la api
-  const fetchseguro = async (idseguro) => {
-    await fetch(`${URLapi}${idseguro}`, {
-      method: "GET",
-    })
-      .then((response) => {
-        // console.log(response.ok); // Será true (verdad) si la respuesta es exitosa.
-        // console.log(response.status); // el código de estado = 200 o código = 400 etc.
-        return response.json();
-      })
-      .then((data) => {
-        if (idseguro === 58) {
-          console.log("Respuesta de la data externa--->", data);
-          setSeguro1(data);
-          console.log("Fin del useEffect");
-        }
-        if (idseguro === 59) {
-          console.log("Respuesta de la data externa--->", data);
-          setSeguro2(data);
-          console.log("Fin del useEffect");
-        }
-      })
-      //mostrarnos un error en caso de que lo tuvieramos
-      .catch((err) => console.log("muestro error -->", err));
-  };
+  
 
-  useEffect(() => {
-    //Llamando a la API con useEffect
-    fetchseguro(58);
-    fetchseguro(59);
-  }, []);
-
+  //Componente para la carta del seguro
   const SeguroCard = (props) => {
-    return <div>{seguro1 ? <p>{seguro1.insurance.name}</p> : null}</div>;
+    //Aqui se guarda el seguro extraido del fetch
+    const [seguro, setSeguro] = useState();
+
+    //funcion Connexion al endpoint
+    const fetchseguro = async (idseguro) => {
+      await fetch(`${URLapi}${idseguro}`, {
+        method: "GET",
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setSeguro(data);
+        })
+        //mostrarnos un error en caso de que lo tuvieramos
+        .catch((err) => console.log("Muestro error -->", err));
+    };
+
+    //Conectando a la API apenas se inicie el componente
+    useEffect(() => {
+      fetchseguro(props.buttonSeguroSelected);
+    }, [props.buttonSeguroSelected]);
+
+    return <div>{seguro ? (
+      <>
+        <p>{seguro.insurance.name}</p>
+        <p>{seguro.insurance.description}</p>
+        <p>{seguro.insurance.price}</p>
+        <p>{seguro.insurance.image}</p>
+      </>
+      ): null}</div>;
   };
 
+  //Asignamos al estado del select cuando este cambia
   const handleSelectSeguro = (e) => {
-    console.log(e.target.value);
     setSeguroIdSelected(e.target.value);
   };
-
+  //Asignamos al estado del button cuando este cambia
   const handleButtonSeguro = (e) => {
     e.preventDefault();
     setButtonSeguroSelected(seguroIdSelected);
-    console.log(buttonSeguroSelected);
   };
+
+
+
+
+
+
+
   return (
     <div className="App">
       <h1>bicevida new</h1>
